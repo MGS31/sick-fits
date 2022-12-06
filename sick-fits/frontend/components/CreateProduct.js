e1f45dfc597fadf75f0f95377445cda230d01a1d
Matt Stankovich <matt.stankovich@cbc.ca>
2022-12-06 17:24:49 -0500

    use mutation to add products to the web app

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,6 +1,30 @@
 import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import DisplayError from './ErrorMessage';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -9,12 +33,23 @@ export default function CreateProduct() {
     description: 'These are the best shoes ever',
   });
 
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
+        // submit the input fields to the backend.
+        const res = await createProduct();
+        clearForm();
       }}>
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input type="file" id="image" name="image" onChange={handleChange} />
